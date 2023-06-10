75384677c62917c45d312ed330c2437369a2938b
coffee <nasyroff.rus@gmail.com>
2023-06-10 16:25:42 +0400

    Add vuetify

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
+const path = require('path');
+
 const { defineConfig } = require('@vue/cli-service');
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
 
 module.exports = defineConfig({
-  transpileDependencies: true,
+  transpileDependencies: ['vuetify'],
+  configureWebpack: {
+    plugins: [new VuetifyLoaderPlugin()],
+    resolve: {
+      alias: {
+        '@/': path.resolve(__dirname, 'src'),
+      },
+    },
+  },
 });
