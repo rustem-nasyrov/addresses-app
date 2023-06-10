71b7c602409cd8e74235138fc4531d00fe884aaf
coffee <nasyroff.rus@gmail.com>
2023-06-10 18:41:42 +0400

    Add default layout and use it

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,33 @@
 <template>
-  <div>
-    <nav>
-      <router-link to="/">Home</router-link>
-    </nav>
-    <router-view />
-  </div>
+  <default-layout>
+    <template #header>
+      <app-header :tabs="tabs" />
+    </template>
+
+    <v-container fluid>
+      <router-view />
+    </v-container>
+  </default-layout>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+import AppHeader from '@/components/layout/AppHeader.vue';
+import DefaultLayout from '@/layouts/DefaultLayout.vue';
+
+export default Vue.extend({
+  name: 'App',
+
+  components: { AppHeader, DefaultLayout },
+
+  computed: {
+    tabs() {
+      return [
+        { to: '/', label: 'О задании' },
+        { to: '/map', label: 'Карта ' },
+      ];
+    },
+  },
+});
+</script>
---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,9 @@
+<template functional>
+  <v-app>
+    <slot name="header" />
+
+    <v-main>
+      <slot />
+    </v-main>
+  </v-app>
+</template>
