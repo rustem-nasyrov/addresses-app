7061e4347da3953fee538b344e768ef89731490a
coffee <nasyroff.rus@gmail.com>
2023-06-11 00:44:32 +0400

    Add geolocation getter method

---
+++ src/utils/index.ts
@@ -0,0 +1,19 @@
+import { DEFAULT_COORDINATES } from '@/store/modules/map/consts';
+
+export const getUserCoordinates = (cb: (args: unknown[]) => unknown) => {
+  if (!window?.navigator?.geolocation) {
+    cb(DEFAULT_COORDINATES);
+
+    return;
+  }
+
+  const onSuccess = ({ coords }: GeolocationPosition) => {
+    cb([coords.latitude, coords.longitude]);
+  }
+
+  const onError = () => {
+    console.warn(`Can not access to user' geolocation.`);
+  };
+
+  window.navigator.geolocation.getCurrentPosition(onSuccess, onError);
+};
