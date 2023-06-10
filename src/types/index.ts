ccf1052a7fdc0f609effe9e7ec378e141153c2fe
coffee <nasyroff.rus@gmail.com>
2023-06-10 20:19:34 +0400

    Add marker and marker coordinates types

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,14 @@ export type AppBarTab = {
   to: string;
   label: string;
 }
+
+export type MarkerCoordinates = {
+  latitude: string;
+  longitude: string;
+}
+
+export type Marker = {
+  id: string;
+  label: string;
+  coordinates: MarkerCoordinates;
+}
