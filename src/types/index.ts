b09b5947240affadb555e2363d6f04d94b261d9b
coffee <nasyroff.rus@gmail.com>
2023-06-11 22:34:19 +0400

    Add new types; update existing ones

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,10 +3,52 @@ export type AppBarTab = {
   label: string;
 }
 
+export type GeocodeAddressRaw = {
+  road?: string;
+  quarter?: string;
+  city_district?: string;
+  state_district?: string;
+  state?: string;
+  postcode?: string;
+  country?: string;
+  country_code?: string;
+};
+
+export type GeocodeResponseRaw = {
+  place_id: number;
+  licence: string;
+  powered_by: string;
+  osm_type: string;
+  osm_id: number;
+  lat: string;
+  lon: string;
+  display_name: string;
+  address: GeocodeAddressRaw;
+  boundingbox: string[];
+};
+
+export type GeocodeAddress = {
+  road?: string;
+  quarter?: string;
+  cityDistrict?: string;
+  stateDistrict?: string;
+  state?: string;
+  postcode?: string;
+  country?: string;
+  countryCode?: string;
+};
+
+export type GeocodeData = {
+  id: number;
+  latitude: string;
+  longitude: string;
+  displayName: string;
+  address: GeocodeAddress;
+};
+
+
 export type Coordinates = [number, number];
 
-export type Marker = {
-  id: string;
-  label: string;
+export type Marker = GeocodeData & {
   coordinates: Coordinates;
 }
