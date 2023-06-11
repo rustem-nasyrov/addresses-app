59af2c20b52e767e18eb179ca46a3519b4f68a34
coffee <nasyroff.rus@gmail.com>
2023-06-11 22:53:40 +0400

    Add snackbar timeout

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,5 @@
 export const OPEN_STREET_MAP_URL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
 export const MAP_ATTRIBUTION = '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors';
+
+export const SNACKBAR_TIMEOUT = 3000;
