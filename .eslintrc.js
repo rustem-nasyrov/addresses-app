24367af1cb4dd89f26d8d9c00e7620ccfe401c44
coffee <nasyroff.rus@gmail.com>
2023-06-12 13:23:16 +0400

    Update eslint rules; remove prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,14 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/essential',
     'eslint:recommended',
+    'plugin:vue/recommended',
     '@vue/typescript/recommended',
-    'plugin:prettier/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2022,
   },
   rules: {
-    'prettier/prettier': ['error'],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'comma-dangle': ['error', 'always-multiline'],
@@ -24,10 +22,7 @@ module.exports = {
   },
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
         jest: true,
       },
